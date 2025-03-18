8ca773723f3840b7c64bc23af8b6c0098e383c30
Zainds <beat1097@mail.ru>
2025-03-18 14:35:26 +0700

    Add 5 games loop

--- logic.js
+++ logic.js
@@ -1,14 +1,53 @@
+playGame()
+
 function getComputerChoice() {
   random = Math.random();
   if (random < 1 / 3) {
-    return "rock";
+    return "Rock";
   } else if (random < 2 / 3) {
-    return "paper";
+    return "Paper";
   } else {
-    return "scissors";
+    return "Scissors";
   }
 }
 function getHumanChoice() {
-  let choice = prompt("Choose: rock / paper / scissors");
+  let choice = prompt("Choose: Rock / Paper / Scissors");
   return choice;
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) playRound(getHumanChoice(), getComputerChoice());
+
+  console.log(`score: human - ${humanScore}, computer - ${computerScore}`);
+  
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice =
+      humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    console.log(
+      `humanChoice: ${humanChoice}, computerChoice: ${computerChoice}`
+    );
+    if (humanChoice === computerChoice) {
+      console.log(`Tie! ${humanChoice} equals ${computerChoice}`);
+
+      return `Tie! ${humanChoice} equals ${computerChoice}`;
+    } else if (
+      (humanChoice === "Rock" && computerChoice === "Scissors") ||
+      (humanChoice === "Paper" && computerChoice === "Rock") ||
+      (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+      humanScore += 1;
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+
+      return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+      computerScore += 1;
+      console.log(`You loose! ${computerChoice} beats ${humanChoice}`);
+
+      return `You loose! ${computerChoice} beats ${humanChoice}`;
+    }
+  }
 }
